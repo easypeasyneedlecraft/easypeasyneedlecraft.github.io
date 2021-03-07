a3e82a6543c96390645806b22e4daffc2dca022c
Masashi Kobayashi <masashi@masashi.ca>
2021-03-07 01:24:02 -0500

    updated

--- js/script.js
+++ js/script.js
@@ -3,8 +3,44 @@ $(document).ready(function(){
 	$('#slides').superslides({
 		animation: 'fade',
 		play: 5000
-
-
 	})
 
+	var typed = new Typed (".typed", {
+		strings: ["Cross Stitcher", "Mother / Mentor", "Japanese living in Toronto"],
+		typeSpeed: 70,
+		loop: true,
+		startDelay: 2000,
+		showCursor: false,
+	});
+
+	$('.owl-carousel').owlCarousel({
+		    loop:true,
+		    margin:10,
+		    nav:true,
+		    responsive:{
+		        0:{
+		            items:1
+		        },
+		        600:{
+		            items:3
+		        },
+		        1000:{
+		            items:5
+		        }
+		    }
+		})
+
+    $('.chart').easyPieChart({
+    	easing:"easeInOut",
+    	barColor: "#fff",
+    	trackColor: false,
+    	scaleColor: false,
+    	linewidth: 4,
+    	size: 150
+
+    });
+
 });
+
+
+	
